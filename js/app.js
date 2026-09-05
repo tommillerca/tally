f127b921d00f2e550277d46474931adc8dd0aab9
Gwart <gwart@local>
2026-09-05 00:45:35 -0400

    Kit Room poster: the lizard at a real size (108px) in the corner, the Bonehead stood left of centre to make room

--- js/app.js
+++ js/app.js
@@ -9379,7 +9379,7 @@ function footballShelfHtml(ownedCos, coinBal, open = false) {
       <div class="fb-hero">
         <div class="pc-worn fit-body">${avatarLayersHtml({ ...RACK_BASE, ...Object.fromEntries(sold.filter(g => !g.pets).map(g => [g.slot, footballItemId(team.id, g.key)])) },
           { wpnAura: null, skip: ['C'], thumb: bhTierFor(260) })}</div>
-        <span class="fb-hero-pet">${croppedPetImg(FOOTBALL_PETS[0], 64, false, null,
+        <span class="fb-hero-pet">${croppedPetImg(FOOTBALL_PETS[0], 108, false, null,
           Object.fromEntries(sold.filter(g => g.pets).map(g => [g.slot, footballItemId(team.id, g.key)])), 192)}</span>
       </div>
       <div class="tx">
