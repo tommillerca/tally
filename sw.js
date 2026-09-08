ce683268b2ff3163dd933093362b9e34e09856f8
Gwart <gwart@local>
2026-09-07 23:31:16 -0700

    v517: nothing you earned goes missing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Tally service worker: precache the app shell, runtime-cache heavy OCR assets.
-const VERSION = 'tally-v516';
+const VERSION = 'tally-v517';
 const PRECACHE = [
   './',
   './index.html',
